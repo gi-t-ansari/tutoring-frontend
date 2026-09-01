a0d83baedbe70adeebd3bb9b7992c49ad2d7b137
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2026-09-01 21:07:59 +0530

    update: 001-09-2026

---
+++ JavaScript/01-09-2026/index.js
@@ -0,0 +1,45 @@
+const imageInput = document.querySelector("#image");
+const imageElement = document.querySelector("#image-element");
+
+imageInput.addEventListener("change", (event) => {
+  //   console.log(event.target.files[0]);
+  //   console.log(URL.createObjectURL(event.target.files[0]));
+
+  imageElement.setAttribute("src", URL.createObjectURL(event.target.files[0]));
+});
+
+const multiImageInput = document.querySelector("#multiple-image");
+
+multiImageInput.addEventListener("change", (event) => {
+  console.log(event.target.files);
+  //   console.log(event.target.files[2]);
+  const imagesContainerElement = document.querySelector("#multiple-images");
+
+  for (let file in event.target.files) {
+    const imageElement = document.createElement("img");
+    imageElement.style.height = "200px";
+    imageElement.style.width = "100px";
+    imageElement.setAttribute(
+      "src",
+      URL.createObjectURL(event.target.files[file]),
+    );
+
+    imagesContainerElement.append(imageElement);
+  }
+});
+
+// const someObject = { name: "some name", age: 11, gender: "male" };
+
+// for (let key in someObject) {
+//   console.log(key);
+// }
+
+const dateInput = document.querySelector("#date-input");
+dateInput.addEventListener("change", (event) => {
+  console.log(event.target.value);
+});
+
+const timeInput = document.querySelector("#time-input");
+timeInput.addEventListener("change", (event) => {
+  console.log(event.target.value);
+});
